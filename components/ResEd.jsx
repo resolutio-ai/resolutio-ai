e768d7c2a30d75fda6d9e3c4f784b21b1274f647
Bestin John <john@rhrk.uni-kl.de>
2022-03-21 10:00:53 +0100

    Udpated Card Component

--- components/ResEd.jsx
+++ components/ResEd.jsx
@@ -11,6 +11,31 @@ import {
 import React from "react";
 
 const ResEd = () => {
+  const mediumArticle = [
+    {
+      id: 1,
+      title: "PFP NFTs — what rights do they give you ?",
+      description:
+        "To help you understand rights associated with PFP NFTs, we decrypt licenses of three top PFP NFT projects.",
+      imageURL: "https://cdn-images-1.medium.com/max/400/0*kDDbRnbeXWw7MRrl",
+    },
+    {
+      id: 2,
+      title: "Confused about your NFT rights ?",
+      description:
+        "To help you understand rights associated with PFP NFTs, we decrypt licenses of three top PFP NFT projects.",
+      imageURL:
+        "https://cdn-images-1.medium.com/max/400/1*7TCLBjFSypF8VlZS0HitGQ.png",
+    },
+    {
+      id: 3,
+      title: "Death of the NFT Artist",
+      description:
+        "To help you understand rights associated with PFP NFTs, we decrypt licenses of three top PFP NFT projects.",
+      imageURL: "https://cdn-images-1.medium.com/max/400/0*yBv6PZWFaFHmsOjm",
+    },
+  ];
+
   return (
     <Box sx={{ textAlign: "center", mt: 12 }}>
       <Typography variant="h5">Get Educated</Typography>
@@ -19,76 +44,46 @@ const ResEd = () => {
         deal with NFT thefts.
       </Typography>
       <Box sx={{ py: 4 }}>
-        <Grid container>
-          <Grid item sm={4} xs={12}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item sm={4} xs={12}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item sm={4} xs={12}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
+        <Grid container columnSpacing={2}>
+          {mediumArticle.map((article) => {
+            return (
+              <Grid
+                item
+                sm={4}
+                xs={12}
+                key={article.id}
+                sx={{ display: "flex" }}
+              >
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={article.imageURL}
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {article.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {article.description}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
         </Grid>
       </Box>
-      <Button variant="outlined">Read More</Button>
+      <Button
+        variant="outlined"
+        target="_blank"
+        href="https://medium.com/resolutio-nft"
+      >
+        Read More
+      </Button>
     </Box>
   );
 };
