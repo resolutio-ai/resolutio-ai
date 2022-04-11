14067593855d2238171fa093f1230112eafbfb4d
Bestin John <john@rhrk.uni-kl.de>
2022-04-11 16:43:16 +0200

    Fixed Welcome Mobile Issues

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -5,7 +5,9 @@ import comingSoonImage from "../public/coming_soon.svg";
 
 const ComingSoon = () => {
   return (
-    <Box sx={{ mx: "auto", textAlign: "center", maxWidth: "500px" }}>
+    <Box
+      sx={{ mx: "auto", mb: "2rem", textAlign: "center", maxWidth: "500px" }}
+    >
       <h1>Coming soon!</h1>
       <Image src={comingSoonImage} alt="coming Soon Image" />
     </Box>
--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,32 +1,27 @@
 import BalanceIcon from "@mui/icons-material/Balance";
 import ForumIcon from "@mui/icons-material/Forum";
-import { Box, Button, Typography, useTheme } from "@mui/material";
-import Image from "next/image";
+import SchoolIcon from "@mui/icons-material/School";
+import { Box, Grid, IconButton, Typography } from "@mui/material";
 import Link from "next/link";
 import React from "react";
-import Scroll from "../public/icons8-scroll.svg";
 
-const useStyles = (theme) => ({
-  logostyles: {
+const useStyles = () => ({
+  iconStyles: {
     textDecoration: "none",
-    color: theme.palette.primary.main,
-    fontSize: 220,
-  },
-  textLine: {
-    color: theme.palette.primary.contrastText,
-  },
-  iconStyle: {
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
+    fontSize: {
+      xs: "5rem",
+      sm: "7rem",
+      lg: "10rem",
+    },
   },
   iconText: {
-    color: theme.text.primary,
+    cursor: "pointer",
+    fontWeight: "bold",
   },
 });
+
 const Welcome = () => {
-  const theme = useTheme();
-  const styles = useStyles(theme);
+  const styles = useStyles();
 
   return (
     <Box sx={{ textAlign: "center" }}>
@@ -34,43 +29,46 @@ const Welcome = () => {
       <p>
         We are a community of artists, NFT collectors, developers, lawyers and
         other NFT enthusiasts, working together to tackle art theft and
-        demystify NFT rights.{" "}
+        demystify NFT rights.
       </p>
-      <Box
-        fontSize="200px"
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Link href="/res-ed" passHref>
-          <Button sx={styles.iconStyle}>
-            <Image
-              sx={styles.logostyles}
-              src={Scroll}
-              alt="Res Ed"
-              width={200}
-              height={200}
-            />
-            <Typography sx={styles.iconText}>Res Ed</Typography>
-          </Button>
-        </Link>
-        <Link href="/initiate-dispute" passHref>
-          <Button sx={styles.iconStyle}>
-            <BalanceIcon sx={styles.logostyles} fontSize="inherit" />
-            <Typography sx={styles.iconText}>Dispute Resolution</Typography>
-          </Button>
-        </Link>
-        <Link href="https://discord.com/invite/RdN9zWQ4AA" passHref>
-        <a target="_blank">
-          <Button sx={styles.iconStyle}>
-            <ForumIcon sx={styles.logostyles} fontSize="inherit" />
-            <Typography sx={styles.iconText}>Join our community</Typography>
-          </Button>
-          </a>
-        </Link>
-      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Link href="/res-ed" passHref>
+            <Box>
+              <IconButton color="primary">
+                <SchoolIcon sx={styles.iconStyles} />
+              </IconButton>
+              <Typography sx={styles.iconText} color="primary">
+                Res Ed
+              </Typography>
+            </Box>
+          </Link>
+        </Grid>
+        <Grid item xs={4}>
+          <Link href="/initiate-dispute" passHref>
+            <Box>
+              <IconButton color="primary">
+                <BalanceIcon sx={styles.iconStyles} />
+              </IconButton>
+              <Typography sx={styles.iconText} color="primary">
+                Dispute Resolution
+              </Typography>
+            </Box>
+          </Link>
+        </Grid>
+        <Grid item xs={4}>
+          <Link href="https://discord.com/invite/RdN9zWQ4AA" passHref>
+            <Box>
+              <IconButton color="primary">
+                <ForumIcon sx={styles.iconStyles} />
+              </IconButton>
+              <Typography sx={styles.iconText} color="primary">
+                Join our community
+              </Typography>
+            </Box>
+          </Link>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
