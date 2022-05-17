2d5e0bca39d6faf2ee750b7212ae2e94d303c7db
Bestin John <john@rhrk.uni-kl.de>
2022-05-17 11:26:28 +0200

    Updated Loading Condition

--- pages/image-verification.js
+++ pages/image-verification.js
@@ -69,27 +69,30 @@ const ImageVerification = () => {
           Search
         </Button>
       </Box>
-      {open && <CircularProgress color="inherit" size={20} />}
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={6}>
-          {images?.map((item, index) => (
-            <Grid item xs={6} sm={4} md={3} key={index}>
-              <Image
-                height={250}
-                width={250}
-                layout="responsive"
-                src={item.cached_file_url}
-                alt={item.title}
-              />
-              <ImageListItemBar
-                title={`Similarity: ${item.similarity}`}
-                subtitle={<span>Chain: {item.chain}</span>}
-                position="below"
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      {open ? (
+        <CircularProgress color="inherit" size={20} />
+      ) : (
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={6}>
+            {images?.map((item, index) => (
+              <Grid item xs={6} sm={4} md={3} key={index}>
+                <Image
+                  height={250}
+                  width={250}
+                  layout="responsive"
+                  src={item.cached_file_url}
+                  alt={item.title}
+                />
+                <ImageListItemBar
+                  title={`Similarity: ${item.similarity}`}
+                  subtitle={<span>Chain: {item.chain}</span>}
+                  position="below"
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      )}
     </Box>
   );
 };
