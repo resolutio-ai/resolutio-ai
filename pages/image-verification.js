7650cb1a1eefd64a499c6b155953854ef696f157
Bestin John <john@rhrk.uni-kl.de>
2022-05-17 09:48:02 +0200

    Added Button For search

--- pages/image-verification.js
+++ pages/image-verification.js
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   CircularProgress,
   Grid,
   ImageListItemBar,
@@ -25,13 +26,10 @@ const ImageVerification = () => {
   const [open, setOpen] = React.useState(false);
   const loading = open && images.length === 0;
 
-  const handleURLChange = async (event) => {
+  const handleSearch = async () => {
     setOpen(true);
-    console.log(event.target.value);
-    setURL(event.target.value);
-
     const res = await instance.post("duplicates/urls", {
-      url: event.target.value,
+      url: imageURL,
       page_number: 1,
       page_size: 50,
       threshold: 0.9,
@@ -45,32 +43,39 @@ const ImageVerification = () => {
     };
   };
 
+  const handleURLChange = (event) => {
+    setURL(event.target.value);
+  };
+
   return (
     <Box sx={{ minHeight: "70vh" }}>
       <h2>Image Verification</h2>
-      <Box sx={{ flexGrow: 1, margin: "15px 0 15px", width: "97%" }}>
+      <Box sx={{ display: "flex", my: "2rem" }}>
         <TextField
           id="outlined-basic"
-          columns={{ xs: 12, md: 12 }}
           sx={{ width: "100%" }}
           label="Image URL"
           variant="outlined"
           value={imageURL}
           onChange={handleURLChange}
         />
+        <Button
+          variant="contained"
+          sx={{ ml: "1rem", px: "2rem" }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
       </Box>
       {loading ? <CircularProgress color="inherit" size={20} /> : null}
       <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-        >
+        <Grid container spacing={6}>
           {images?.map((item, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
+            <Grid item xs={6} sm={4} md={3} key={index}>
               <Image
-                height={150}
-                width={150}
+                height={250}
+                width={250}
+                layout="responsive"
                 src={item.cached_file_url}
                 alt={item.title}
               />
