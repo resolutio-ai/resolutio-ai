401829f0fb0a1eff2bf4db6f19144c776708c875
Bestin John <john@rhrk.uni-kl.de>
2022-05-20 19:53:33 +0200

    Create Meta Component

---
+++ components/seo/Meta.jsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+
+const Meta = ({
+  title,
+  keywords,
+  description,
+  ogTitle,
+  ogType,
+  ogUrl,
+  ogImage,
+}) => {
+  return (
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
+      <meta name="keywords" content={keywords}></meta>
+      <meta name="description" content={description}></meta>
+      <meta property="og:title" content={ogTitle} />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:url" content={ogUrl} />
+      <meta property="og:image" content={ogImage} />
+      <meta charSet="utf-8"></meta>
+      <link rel="icon" href="/favicon.ico"></link>
+      <title>{title}</title>
+    </Head>
+  );
+};
+Meta.defaultProps = {
+  title: "resolutio",
+  keywords:
+    "nft, resolutio, resolutio nft, resolutio token, resolutio token nft",
+  description: "",
+  ogTitle: "resolutio",
+};
+export default Meta;
