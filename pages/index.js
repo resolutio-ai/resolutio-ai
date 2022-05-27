43b008d45d973966a4365e01ba0d431e1d4ce98d
Bestin John <john@rhrk.uni-kl.de>
2022-05-28 01:35:59 +0200

    Updated Welcome Section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import { Box, Button, Typography } from "@mui/material";
+import Link from "next/link";
+import BetaBanner from "../components/BetaBanner";
 import OurTeam from "../components/OurTeam";
 import ResEd from "../components/ResEd";
 import Meta from "../components/seo/Meta";
+import { IMAGE_VERIFICATION_HEADING } from "../constants/strings";
 
 const Home = () => {
   return (
@@ -10,6 +14,24 @@ const Home = () => {
         keywords="NFT theft, NFT search, NFT scam, NFT rights, NFT duplication, NFT disputes, Blockchain dispute resolution, Sneha Vijyan, NFT, resolutio, Zareen Reza, Res Ed, Res Educate"
       />
       {/* <Welcome /> */}
+      <>
+        <Box sx={{ textAlign: "center", my: "2rem" }}>
+          <Typography variant="h5">
+            {IMAGE_VERIFICATION_HEADING}
+            <BetaBanner />
+          </Typography>
+          {/* <Typography variant="body1">{IMAGE_VERIFICATION_DESC}</Typography> */}
+          <Typography variant="body1">
+            Verify whether your art has been tokenized or your NFT duplicated
+            without consent on <b>Ethereum</b>, <b>Polygon</b> or <b>Tezos</b>.
+          </Typography>
+          <Link href="/image-verification" passHref>
+            <Button variant="outlined" sx={{ mt: "1rem" }}>
+              VERIFY NOW
+            </Button>
+          </Link>
+        </Box>
+      </>
       <ResEd />
       <OurTeam />
     </div>
