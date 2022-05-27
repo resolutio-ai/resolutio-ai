6855ad0f80cebade3565fcf528dca1d31ddcbfa9
Bestin John <john@rhrk.uni-kl.de>
2022-05-28 01:15:57 +0200

    Removed Welcome

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import OurTeam from "../components/OurTeam";
 import ResEd from "../components/ResEd";
 import Meta from "../components/seo/Meta";
-import Welcome from "../components/Welcome";
 
 const Home = () => {
   return (
@@ -10,7 +9,7 @@ const Home = () => {
         title="resolutio"
         keywords="NFT theft, NFT search, NFT scam, NFT rights, NFT duplication, NFT disputes, Blockchain dispute resolution, Sneha Vijyan, NFT, resolutio, Zareen Reza, Res Ed, Res Educate"
       />
-      <Welcome />
+      {/* <Welcome /> */}
       <ResEd />
       <OurTeam />
     </div>
