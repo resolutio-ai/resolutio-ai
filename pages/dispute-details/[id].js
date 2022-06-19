963920f4a45efe240d3f8f67619396b4b866676e
Bestin John <bestinjohn1993@gmail.com>
2022-06-19 23:58:43 +0200

    Added Details Page

--- pages/dispute-details/[id].js
+++ pages/dispute-details/[id].js
@@ -1,9 +1,40 @@
+import { Box, Button, Grid, Typography } from "@mui/material";
 import Meta from "../../components/seo/Meta";
 const DisputeDetails = () => {
   return (
     <>
       <Meta title="Dispute Details" />
-      <div>Dispute Details</div>
+      <Box sx={{ textAlign: "center", mb: 10 }}>
+        <Typography variant="h1">DA01</Typography>
+        <Grid container spacing={2}>
+          <Grid item md={4} sm={6} xs={12}>
+            <Button variant="contained" color="secondary">
+              Facts
+            </Button>
+          </Grid>
+          <Grid item md={4} sm={6} xs={12}>
+            <Button variant="contained" color="secondary">
+              Evidence
+            </Button>
+          </Grid>
+          <Grid item md={4} sm={6} xs={12}>
+            <Button variant="contained" color="secondary">
+              Arbiter Tools
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ textAlign: "center", mb: 4 }}>
+        <Typography variant="h1">VOTE</Typography>
+        <Box>
+          <Button variant="contained" color="secondary" sx={{ mr: 4 }}>
+            Invalidate NFT
+          </Button>
+          <Button variant="contained" color="primary">
+            Validate NFT
+          </Button>
+        </Box>
+      </Box>
     </>
   );
 };
