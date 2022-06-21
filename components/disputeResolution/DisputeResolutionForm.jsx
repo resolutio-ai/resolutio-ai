3092f540b6549e83a347486dc8236ee5688a1844
Anish <anishpraveen7@gmail.com>
2022-06-21 16:36:16 +0200

    File upload to IPFS via nft.storage

--- components/disputeResolution/DisputeResolutionForm.jsx
+++ components/disputeResolution/DisputeResolutionForm.jsx
@@ -1,7 +1,25 @@
-import { Box, Button, Grid, TextField } from "@mui/material";
+import { Alert, Box, Button, Grid, IconButton, Snackbar, Stack, TextField } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import { jsPDF } from "jspdf";
 import { useCallback, useState } from "react";
 import AttachEvidence from "./AttachEvidence";
+
+import ReactPDF from '@react-pdf/renderer';
+import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+import { useEffect } from 'react';
+import { create } from "ipfs-http-client";
+
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+// const client = create('https://ipfs.infura.io:5001/api/v0');
+
+import { NFTStorage, File } from 'nft.storage'
+const client = new NFTStorage({
+  endpoint: 'https://api.nft.storage',
+  token: process.env.NEXT_PUBLIC_NFT_STORAGE_IPFS_KEY
+})
 
 const defaultValues = {
   nft_id: "",
@@ -15,28 +33,129 @@ const defaultValues = {
 
 const DisputeResolutionForm = () => {
   const [formValues, setFormValues] = useState(defaultValues);
+  const [isClient, setIsClient] = useState(false)
+  const [openAlert, setOpenAlert] = useState(false);
+  const [openLoader, setOpenLoader] = useState(false);
+
+  const handleOpenAlert = () => {
+    setOpenAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
+
+
+  const handleCloseLoader = () => {
+    setOpenLoader(false);
+  };
+  const handleOpenLoader = () => {
+    setOpenLoader(true);
+  };
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  async function uploadToIpfs(formValues) {
+    handleOpenLoader();
+    // e.preventDefault();
+    let pdfContent;
+    try {
+      pdfContent = [
+        { text: "NFT ID", value: formValues["nft_id"] },
+        { text: "NFT URL", value: formValues["nft_url"] },
+        { text: "Marketplace", value: formValues["marketplace"] },
+        {
+          text: "Information Pertaining to the Concerned Parties",
+          value: formValues["info"],
+        },
+        {
+          text: "Subject Matter(i.e. Art, Music, Document, etc.)",
+          value: formValues["subject"],
+        },
+        { text: "Case Details", value: formValues["case_details"] },
+      ];
+      console.log(pdfContent);
+    } catch (error) {
+      console.log('error in data');
+      handleCloseLoader();
+      handleOpenAlert();
+    }
+    try {
+      let fileArray = [
+        new File([`Resolutio case created on ${new Date().toTimeString()}`], 'description.txt'),
+        new File([JSON.stringify(pdfContent, null, 2)], 'formData.json')
+      ];
+      if (formValues.files.length > 0) {
+        formValues.files.forEach(item => {
+          fileArray.push(new File([item], item?.name))
+        });
+      }
+      const cid = await client.storeDirectory(
+
+        fileArray
+      )
+      console.log(cid)
+      console.log(`https://ipfs.io/ipfs/${cid}`)
+      if (cid) {
+        clearForm();
+      }
+    } catch (error) {
+      console.log(error);
+      console.log(error.message);
+      handleOpenAlert();
+      handleCloseLoader();
+    }
+   
+    handleCloseLoader();
+
+  }
 
   const createPDF = useCallback(() => {
-    const pdf = new jsPDF();
-    const pdfContent = [
-      { text: "NFT ID", key: "nft_id" },
-      { text: "NFT URL", key: "nft_url" },
-      { text: "Marketplace", key: "marketplace" },
-      {
-        text: "Information Pertaining to the Concerned Parties",
-        key: "info",
-      },
-      {
-        text: "Subject Matter(i.e. Art, Music, Document, etc.)",
-        key: "subject",
-      },
-      { text: "Case Details", key: "case_details" },
-    ];
-    pdfContent.map(({ text, key }, indx) => {
-      pdf.text(`${text}: ${formValues[key]}`, 10, 10 + indx * 10);
-    });
-    pdf.save("evidence.pdf");
+    uploadToIpfs(formValues);
+
+    return;
+
+
+    // ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`);
+
   }, [formValues]);
+
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'row',
+      backgroundColor: '#E4E4E4'
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1
+    },
+    image: {
+      width: 500,
+      height: 500,
+    },
+  });
+  // Creating PDF : Future use
+  const MyDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <Image
+          style={styles.image}
+          src="https://image.shutterstock.com/image-photo/tiny-floating-house-on-lake-600w-1980476267.jpg"
+        />
+        <View style={styles.section}>
+          <Text>NFT ID</Text>
+          <Text>NFT URL</Text>
+          <Text>Marketplace</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
+  );
 
   const handleInputChange = useCallback(
     (e) => {
@@ -58,7 +177,7 @@ const DisputeResolutionForm = () => {
       event.preventDefault();
       console.log(formValues);
       createPDF();
-      clearForm();
+      // clearForm();
     },
     [formValues, createPDF, clearForm]
   );
@@ -81,6 +200,44 @@ const DisputeResolutionForm = () => {
         margin: "2rem auto",
       }}
     >
+      {openAlert && (
+        <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
+          <Alert variant="filled" severity="error">
+            Error creating dispute. Please try again.
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              sx={{ p: 0.5 }}
+              onClick={handleCloseAlert}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Alert>
+        </Snackbar>
+      )}
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={openLoader}
+      >
+        <Stack spacing={2} alignItems="center">
+          < >Please wait while dispute is being created</>
+          <CircularProgress color="primary" />
+        </Stack>
+
+
+
+      </Backdrop>
+      {/* {isClient && <PDFViewer>
+        <MyDocument />
+      </PDFViewer>}
+
+      {isClient && (
+        <PDFDownloadLink document={<MyDocument />} fileName="fee_acceptance.pdf">
+          {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
+        </PDFDownloadLink>
+      )} */}
+
+      {/* https://react-pdf.org/ */}
       <form onSubmit={handleFormSubmit}>
         <Grid container spacing={2} direction="column">
           <Grid item>
