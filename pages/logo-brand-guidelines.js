584ae7cedc3884226d127c2f516159ede28a371c
Bestin John <bestinjohn1993@gmail.com>
2022-08-14 20:25:22 +0200

    Added Brand Guidelines pages

---
+++ pages/logo-brand-guidelines.js
@@ -0,0 +1,11 @@
+import Meta from "../components/seo/Meta";
+
+const LogoBrandGuidelines = () => {
+  return (
+    <>
+      <Meta title="Resolutio Logo and Brand Guidelines" />
+    </>
+  );
+};
+
+export default LogoBrandGuidelines;
