3b26996e0a823e38ac1bf8b6b000cadd6dbf198f
Bestin John <bestinjohn1993@gmail.com>
2022-11-06 00:40:27 +0100

    Added Code for Voting

--- pages/dispute-details/[id].js
+++ pages/dispute-details/[id].js
@@ -7,19 +7,21 @@ import {
   Typography,
 } from "@mui/material";
 import { useRouter } from "next/router";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import CountDownTimer from "../../components/CountDownTimer";
 import StakingDialog from "../../components/dialogs/StakingDialog";
 import NotArbiter from "../../components/NotArbiter";
 import RenderOnArbiter from "../../components/RenderOnArbiter";
 import Meta from "../../components/seo/Meta";
 import SmartLink from "../../components/SmartLink";
 import Unauthorized from "../../components/Unauthorized";
+import { CAN_VOTE, CREATED } from "../../constants/constants";
 import { useResolutioContext } from "../../context/ResolutioContext";
 import DisputePool from "../../integrations/DisputePool";
 
 const DisputeDetails = () => {
   const router = useRouter();
-  const { address } = useResolutioContext();
+  const { address, isArbiter } = useResolutioContext();
   const { id } = router.query;
   const [dispute, setDispute] = useState({
     title: "",
@@ -37,7 +39,12 @@ const DisputeDetails = () => {
     additionalDetails: null,
   });
   const [isStakingDialogOpen, setStakingDialogOpen] = useState(false);
+  const [isVoted, setVoted] = useState(false);
 
+  const isPageViewable = useMemo(
+    () => isArbiter || dispute.creator === address,
+    [address, dispute, isArbiter]
+  );
   const handleStakingDialogOpen = useCallback(() => {
     setStakingDialogOpen(true);
   }, []);
@@ -51,8 +58,8 @@ const DisputeDetails = () => {
       if (!id) return;
       try {
         const disputeSystem = new DisputePool();
-        const response = await disputeSystem.joinDisputePool(id);
-        console.log(response);
+        await disputeSystem.joinDisputePool(id);
+        setDispute((prev) => ({ ...prev, hasStaked: true }));
         handleStakingDialogClose();
       } catch (error) {
         console.log(error);
@@ -61,6 +68,31 @@ const DisputeDetails = () => {
     };
     asyncJoinDisputePool();
   }, [handleStakingDialogClose, id]);
+
+  const handleVoting = useCallback(
+    (vote) => {
+      const votingAsync = async () => {
+        if (!id) return;
+        try {
+          const disputeSystem = new DisputePool();
+          await disputeSystem.vote(vote, id);
+          setVoted(true);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      votingAsync();
+    },
+    [id]
+  );
+
+  const handleValidateDispute = useCallback(() => {
+    handleVoting(1);
+  }, [handleVoting]);
+
+  const handleInvalidateDispute = useCallback(() => {
+    handleVoting(2);
+  }, [handleVoting]);
 
   useEffect(() => {
     const asyncGetDisputeById = async () => {
@@ -104,7 +136,7 @@ const DisputeDetails = () => {
   return (
     <>
       <Meta title="Dispute Details" />
-      <RenderOnArbiter>
+      {isPageViewable && (
         <Card sx={{ my: 4 }}>
           <CardContent>
             <Box>
@@ -140,20 +172,22 @@ const DisputeDetails = () => {
                       Evidence
                     </Button>
                   </SmartLink>
-                  <SmartLink
-                    href="https://znreza-blockchain-transaction-search-app-4pp5e7.streamlitapp.com/"
-                    isExternal={true}
-                  >
-                    <Button variant="contained" color="secondary">
-                      Arbiter Tools
-                    </Button>
-                  </SmartLink>
+                  <RenderOnArbiter>
+                    <SmartLink
+                      href="https://znreza-blockchain-transaction-search-app-4pp5e7.streamlitapp.com/"
+                      isExternal={true}
+                    >
+                      <Button variant="contained" color="secondary">
+                        Arbiter Tools
+                      </Button>
+                    </SmartLink>
+                  </RenderOnArbiter>
                 </Box>
               </>
             </Box>
           </CardContent>
           <CardActions sx={{ justifyContent: "center" }}>
-            {!dispute.hasStaked && (
+            {!dispute.hasStaked && dispute.state === CREATED && (
               <Box sx={{ textAlign: "center" }}>
                 <Typography variant="h5">
                   Would you like to be an arbiter for this case ?
@@ -168,22 +202,35 @@ const DisputeDetails = () => {
                 </Button>
               </Box>
             )}
-            <Box>
-              {/*         <Box sx={{ textAlign: "center", mb: 4 }}>
-          <Typography variant="h1">VOTE</Typography>
-          <CountDownTimer expiryTimestamp={1656530040208} />
-          <Box>
-            <Button variant="contained" color="secondary" sx={{ mr: 4 }}>
-              Invalidate NFT
-            </Button>
-            <Button variant="contained" color="primary">
-              Validate NFT
-            </Button>
-          </Box>
-        </Box> */}
-            </Box>
+            {dispute.state === CAN_VOTE && !isVoted && (
+              <Box>
+                <Box sx={{ textAlign: "center", mb: 4 }}>
+                  <Typography variant="h1">VOTE</Typography>
+                  <CountDownTimer expiryTimestamp={1656530040208} />
+                  <Box>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      sx={{ mr: 4 }}
+                      onClick={handleInvalidateDispute}
+                    >
+                      Invalidate NFT
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleValidateDispute}
+                    >
+                      Validate NFT
+                    </Button>
+                  </Box>
+                </Box>
+              </Box>
+            )}
           </CardActions>
         </Card>
+      )}
+      <RenderOnArbiter>
         <StakingDialog
           open={isStakingDialogOpen}
           onClose={handleStakingDialogClose}
@@ -191,7 +238,7 @@ const DisputeDetails = () => {
           description={dispute.description}
         />
       </RenderOnArbiter>
-      <NotArbiter />
+      {!isPageViewable && <NotArbiter />}
       <Unauthorized />
     </>
   );
