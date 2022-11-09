aadad59c01fbed713d714a7121ea14f5ffb9cd8f
Bestin John <bestinjohn1993@gmail.com>
2022-11-09 20:49:31 +0100

    Added N progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,34 @@
 import { CacheProvider } from "@emotion/react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Router from "next/router";
+import NextNProgress from "nextjs-progressbar";
+import NProgress from "nprogress";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 import NotistackWrapper from "../components/NotistackWrapper";
 import { ResolutioContextProvider } from "../context/ResolutioContext";
+import { primaryMain } from "../styles/colors";
 import "../styles/globals.css";
 import resolutioTheme from "../styles/theme/resolutioTheme";
 import createEmotionCache from "../utility/createEmotionCache";
 
 const clientSideEmotionCache = createEmotionCache();
+NProgress.configure({ showSpinner: false });
 
 const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => NProgress.start());
+    Router.events.on("routeChangeComplete", () => NProgress.done());
+    Router.events.on("routeChangeError", () => NProgress.done());
+    return () => {
+      Router.events.off("routeChangeStart");
+      Router.events.off("routeChangeComplete");
+      Router.events.off("routeChangeError");
+    };
+  }, []);
 
   return (
     <NotistackWrapper>
@@ -20,6 +37,7 @@ const MyApp = (props) => {
           <ThemeProvider theme={resolutioTheme}>
             <CssBaseline />
             <Layout>
+              <NextNProgress color={primaryMain} />
               <Component {...pageProps} />
             </Layout>
           </ThemeProvider>
