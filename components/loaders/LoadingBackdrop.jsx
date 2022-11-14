bcbd0d57b89b4a32dc93d1f6a0b76064b2f66d21
Bestin John <bestinjohn1993@gmail.com>
2022-11-14 21:32:23 +0100

    Added New loader Icon

--- components/loaders/LoadingBackdrop.jsx
+++ components/loaders/LoadingBackdrop.jsx
@@ -1,16 +1,20 @@
-import { Backdrop, CircularProgress, Stack } from "@mui/material";
+import { Backdrop, Stack, Typography, useTheme } from "@mui/material";
+import { HashLoader } from "react-spinners";
 import { useResolutioBackdropContext } from "../../context/ResolutioBackdropContext";
-
 const LoadingBackdrop = () => {
+  const theme = useTheme();
   const { backdropOpen, backdropMsg } = useResolutioBackdropContext();
+
   return (
     <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={backdropOpen}
     >
       <Stack spacing={2} alignItems="center">
-        {backdropMsg}
-        <CircularProgress color="primary" />
+        <HashLoader color={theme.palette.primary.dark} />
+        <Typography variant="body1" sx={{ color: "white" }}>
+          {backdropMsg}
+        </Typography>
       </Stack>
     </Backdrop>
   );
