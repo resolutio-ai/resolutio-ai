711f41b9ad797ca74dba296565e8bd386e1ad7a2
Bestin John <bestinjohn1993@gmail.com>
2022-11-15 01:19:32 +0100

    Updated Dispute details

--- components/disputeDetails/DisputeInfomation.jsx
+++ components/disputeDetails/DisputeInfomation.jsx
@@ -1,6 +1,47 @@
 import { Box, Typography } from "@mui/material";
+import { useMemo } from "react";
 
 const DisputeInfomation = ({ dispute }) => {
+  const disputeInfo = useMemo(
+    () => [
+      { label: "Victim", value: dispute.creator },
+      { label: "NFT ID", value: dispute.additionalDetails?.nftID ?? "" },
+      {
+        label: "NFT URL",
+        value: dispute.additionalDetails?.nftURL ?? "",
+      },
+      {
+        label: "Marketplace",
+        value: dispute.additionalDetails?.marketplace ?? "",
+      },
+
+      {
+        label: "Information",
+        value: dispute.additionalDetails?.info ?? "",
+      },
+
+      {
+        label: "Subject",
+        value: dispute.additionalDetails?.subject ?? "",
+      },
+
+      {
+        label: "Details",
+        value: dispute.additionalDetails?.details ?? "",
+      },
+
+      {
+        label: "Additional Information",
+        value: dispute.additionalDetails?.additionalInfo ?? "",
+      },
+      {
+        label: "Number of Attached Files",
+        value: dispute.additionalDetails?.attachedFiles ?? "",
+      },
+    ],
+    [dispute]
+  );
+
   return (
     <Box>
       <Typography
@@ -11,18 +52,14 @@ const DisputeInfomation = ({ dispute }) => {
         <Typography variant="h5" sx={{ textAlign: "center" }}>
           {dispute.description}
         </Typography>
-        <Typography variant="body1" sx={{ mt: 2 }}>
-          <strong>Victim:</strong> {dispute.creator}
-        </Typography>
-        {dispute.additionalDetails &&
-          Object.keys(dispute.additionalDetails).map((key) => {
-            return (
-              <Typography variant="body1" sx={{ mt: 2 }} key={key}>
-                <strong>{`${key}: `}</strong>
-                {dispute.additionalDetails[key]}
-              </Typography>
-            );
-          })}
+        {disputeInfo.map((info, index) => {
+          return (
+            <Typography variant="body1" sx={{ mt: 2 }} key={index}>
+              <strong>{`${info.label}: `}</strong>
+              {info.value}
+            </Typography>
+          );
+        })}
       </Box>
     </Box>
   );
