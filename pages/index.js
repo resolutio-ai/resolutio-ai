84f53801d63818889bbb3dbedcd1e78117b2de07
Anish Praveen <anishpraveen7@gmail.com>
2023-03-12 15:56:52 +0100

    Hiding image search

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
         keywords="NFT theft, NFT search, NFT scam, NFT rights, NFT duplication, NFT disputes, Blockchain dispute resolution, Sneha Vijyan, NFT, resolutio, Zareen Reza, Res Ed, Res Educate, Bestin John, Anish Praveen, Ogubuike Alexandra Ozioma"
         description="A club/home for NFT Community(artists, collectors, developers) where the community collectively owns and operates the platform.Resolutio provides the environment for the Community to come together, engage, and uplift themselves, to help each other and protect each other from harm.Resolutio provides the resources and tools, and extends help to the Community so they can promote and protect each other. [resolutio protects, but by helping the community protect themselves]"
       />
-      <ImageVerification />
+      {/* <ImageVerification /> */}
       {/* <InitiateDispute /> */}
       <ResEd />
       <OurTeam />
