0f3e577c678e1823286508121bc9a6af9774b647
Bestin John <bestin.john@iese.fraunhofer.de>
2023-11-02 21:57:42 +0100

    Removed Boilerplate code

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>Header</header>
+        {children}
+        <footer>Footer</footer>
+      </body>
     </html>
   );
 }
