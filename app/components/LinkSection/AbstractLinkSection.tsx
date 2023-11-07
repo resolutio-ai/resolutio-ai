a0972e2860820826afd29ff22d868d4277759f38
Bestin John <bestinjohn1993@gmail.com>
2023-11-07 14:17:49 +0100

    Fixes Container Height

--- app/components/LinkSection/AbstractLinkSection.tsx
+++ app/components/LinkSection/AbstractLinkSection.tsx
@@ -19,7 +19,7 @@ export const AbstractLinkSection: FC<AbstractLinkSectionProps> = ({
 }) => {
   return (
     <div className='abstract-link-section py-16'>
-      <div className='container mx-auto px-4'>
+      <div className='res-container'>
         <h1 className='font-primary-heading mb-4'>{heading}</h1>
         <p className='mb-10'>{description}</p>
         <button className='btn btn-outline'>{btnText}</button>
