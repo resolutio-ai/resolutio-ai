98019b82380986688c561dc38a71e639c147109a
Bestin John <bestinjohn1993@gmail.com>
2023-11-13 22:59:47 +0100

    Fixed Build Issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,12 @@
 import { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://resolutio.ai/',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-  ];
-}
+const sitemap = (): MetadataRoute.Sitemap => [
+  {
+    url: 'https://resolutio.ai/',
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 1,
+  },
+];
+
+export default sitemap;
