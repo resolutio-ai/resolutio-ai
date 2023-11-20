d08d802c77a4d714265fc8d59a77b8253d825ea7
Bestin John <bestinjohn1993@gmail.com>
2023-11-21 00:06:55 +0100

    Improves SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import {
   Blogs,
   CommunityLink,
@@ -9,10 +10,23 @@ import {
   ResolveDispute,
   UnderstandYourRights,
 } from './components';
+import { GA_TRACKING_ID } from './settings';
 
 const Home = () => {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
       <OurStory />
       <EvidenceLink />
       <ResolveDispute />
