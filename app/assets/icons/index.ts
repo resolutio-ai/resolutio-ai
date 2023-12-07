d14bf40e5c124048e5747cd019418a910d576178
tamuno777 <davisbeulah07@gmail.com>
2023-12-07 10:56:14 -0800

    folder structure

---
+++ app/assets/icons/index.ts
@@ -0,0 +1,18 @@
+import messages from "./message-square-02.png"
+import notification from "./bell-01.png"
+import evidencForm from "./Frame.png"
+import saved from "./bookmark.png"
+import support from "./annotation-question.png"
+import profile from "./user-01.png"
+import search from "./search.png"
+
+
+export{
+    messages,
+    notification,
+    evidencForm,
+    saved,
+    support,
+    profile,
+    search
+}
