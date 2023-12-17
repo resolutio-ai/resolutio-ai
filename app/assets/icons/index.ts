3a7bc11e8144e4e5309782b7881abcb0a2b3fdf4
Winnie Davis <63395082+tamuno777@users.noreply.github.com>
2023-12-17 19:31:10 +0100

    Feature/evidence feed home UI (#55)
    * feed ui
    
    * changes

---
+++ app/assets/icons/index.ts
@@ -0,0 +1,40 @@
+import evidencForm from './Frame.png';
+import supportIcon from './annotation-question.png';
+import notificationIcon from './bell-01.png';
+import booknnarkIcon from './bookmark.svg';
+import PaintbrushSvg from './brush-01.svg';
+import dropIcon from './chevron-down.png';
+import MoreIcon from './dot.png';
+import eyeIcon from './eye.png';
+import homeBtnSvg from './home.svg';
+import messageIcon from './message-square-02.png';
+import musicNoteIconSvg from './music-note-01.svg';
+import pencilIconSvg from './pencil-line.svg';
+import plusIcon from './plus.png';
+import searchIcon from './search-md.png';
+import starIconSvg from './star-06.svg';
+import profileIcon from './user-01.png';
+import usersIcon from './users-01.png';
+import dropdownIcon from "./dropdown.svg"
+
+export {
+    MoreIcon,
+    dropdownIcon,
+    PaintbrushSvg,
+    booknnarkIcon,
+    dropIcon,
+    evidencForm,
+    eyeIcon,
+    homeBtnSvg,
+    messageIcon,
+    musicNoteIconSvg,
+    notificationIcon,
+    pencilIconSvg,
+    plusIcon,
+    profileIcon,
+    searchIcon,
+    starIconSvg,
+    supportIcon,
+    usersIcon
+};
+
