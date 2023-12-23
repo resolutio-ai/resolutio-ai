eb839f7fa517c8519f2afc00a35f7d57c54ae82d
Winnie Davis <63395082+tamuno777@users.noreply.github.com>
2023-12-23 11:31:15 +0100

    Feature/evidence form (#59)
    * ui
    
    * chore: upgrades dependencies (#56) (#57)
    
    * form ui
    
    * form link
    
    * conflict
    
    * Removes package.json and update pnpm
    
    * form adjustment
    
    * Resolve package.json conflict
    
    * removes package-lock.json
    
    ---------
    
    Co-authored-by: Bestin John <bestii@users.noreply.github.com>
    Co-authored-by: Bestin John <bestinjohn1993@gmail.com>

---
+++ app/components/Evidence/formSections/creatorInput.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+
+interface Creator {
+  id: number;
+  name: string;
+}
+
+interface CreatorInputProps {
+  id: number;
+  value: string;
+  onChange: (id: number, value: string) => void;
+}
+
+const CreatorInput: FC<CreatorInputProps> = ({ id, value, onChange }) => {
+  return (
+    <div>
+      <label className='font-noto-sans text-sm font-bold leading-tight text-gray-600'>
+        Name of Creator
+      </label>
+      <input
+        className='focus:shadow-outline mb-2 flex w-[100%] appearance-none flex-col items-center justify-center gap-2 rounded-md border border-solid border-[#5F437F] px-4 py-3 leading-tight text-gray-700 shadow focus:outline-none'
+        id={`name${id}`}
+        type='text'
+        placeholder='Enter name'
+        value={value}
+        onChange={(e) => onChange(id, e.target.value)}
+      />
+    </div>
+  );
+};
+
+interface CreatorsListProps {
+  creators: Creator[];
+  onAddCreator: () => void;
+  onNameChange: (id: number, value: string) => void;
+}
+
+const CreatorsList: FC<CreatorsListProps> = ({ creators, onAddCreator, onNameChange }) => {
+  return (
+    <div>
+      {creators.map((creator) => (
+        <CreatorInput key={creator.id} id={creator.id} value={creator.name} onChange={onNameChange} />
+      ))}
+      <div onClick={onAddCreator} className='flex cursor-pointer text-right '>
+        <p className='font-weight: 400 w-[100%] px-2  text-base  leading-tight'>
+          <span className='align-center px-2 text-lg text-primary  '>&#43;</span>
+          Add Co-creators
+        </p>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default CreatorsList;
