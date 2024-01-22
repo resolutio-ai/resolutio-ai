aca4739b006c8e7556cda390272386d2295f623b
tamuno777 <davisbeulah07@gmail.com>
2024-01-22 12:36:51 -0800

    api integration

--- app/components/Evidence/Evidenceform/Evidenceform.tsx
+++ app/components/Evidence/Evidenceform/Evidenceform.tsx
@@ -12,8 +12,8 @@ import FileUploadForLicense from '../formSections/fileUploadforLicence';
 import LicenseSelect from '../formSections/licenseSelect';
 import Medium from '../formSections/medium';
 import WorkNameInput from '../formSections/workNameInpute';
-
 import './Evidenceform.scss';
+import { submitEvidence } from '@/app/adapter/browser/formApiService';
 
 const defaultValues: EvidenceFromDto = {
   creators: [{ id: 1, name: '' }],
@@ -30,20 +30,39 @@ const Evidenceform: FC = () => {
     defaultValues: defaultValues,
   });
 
+  const [file, setFile] = useState<File | null>(null);
+  const [licenceFile, setLicenceFile] = useState<File | null>(null);
+  const [workName, setWorkName] = useState('');
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [creators, setCreators] = useState<Creator[]>([{ id: 1, name: '' }]);
   const [selectedLicense, setSelectedLicense] = useState<string>('');
+  const [selectedMedium, setSelectedMedium] = useState<string>('');
+  const [alternativeMedium, setAlternativeMedium] = useState<string>('');
+  const [formSubmissionMessage, setFormSubmissionMessage] =
+    useState<string>('');
 
   const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    // setFile(e.target.files[0]);
+    const uploadedFile = e.target.files?.[0];
+
+    if (uploadedFile) {
+      setFile(uploadedFile);
+    }
   };
 
+  const handleMediumSelected = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedMedium(e.target.value);
+  };
+  const handleAlternativeMedium = (e: ChangeEvent<HTMLInputElement>) => {
+    setAlternativeMedium(e.target.value);
+  };
+  const handleWorkName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWorkName(event.target.value);
+  };
   const addCreator = () => {
     const newId = creators.length + 1;
     setCreators([...creators, { id: newId, name: '' }]);
   };
-
-  const handleNameChange = (id: number, value: string) => {
+  const handleCreatorName = (id: number, value: string) => {
     setCreators((prevCreators) =>
       prevCreators.map((creator) =>
         creator.id === id ? { ...creator, name: value } : creator
@@ -56,45 +75,92 @@ const Evidenceform: FC = () => {
   };
 
   const handleLicenseUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    // Your file upload logic here
+    const uploadedLicenceFile = e.target.files?.[0];
+
+    if (uploadedLicenceFile) {
+      setLicenceFile(uploadedLicenceFile);
+    }
   };
 
-  const onSubmit: SubmitHandler<EvidenceFromDto> = (data) => {
-    console.log(data);
+  const handleFormSubmit: SubmitHandler<EvidenceFromDto> = async () => {
+    try {
+      const formData = new FormData();
+      const metadata = {
+        licenseType: selectedLicense,
+        alternativeMedium: alternativeMedium,
+        medium: selectedMedium,
+        dateOfCreation: selectedDate?.toISOString(),
+        nameOfWork: workName,
+        creatorId: creators,
+      };
+
+      formData.append('metadata', JSON.stringify(metadata));
+      if (file) {
+        formData.append('userworks', file);
+      }
+
+      if (licenceFile) {
+        formData.append('userpersonalizedlicense', licenceFile);
+      }
+      const response = await submitEvidence(formData);
+      console.log('API Response:', response);
+      setFormSubmissionMessage('Form submission was sucessfull ');
+    } catch (error) {
+      console.error('Error:', error);
+      setFormSubmissionMessage(
+        'Form submission unsuccessful .pls try again '
+      );
+    }
     reset();
   };
+
+
 
   return (
     <div className='p-5 lg:p-10'>
       <h3 className='pb-4 text-4xl font-bold tracking-tight text-gray-500'>
         Evidence Form
       </h3>
+
       <form
         className='w-[100%] space-y-6 md:w-[328px]'
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(handleFormSubmit)}
       >
-        <CreatorsList
-          creators={creators}
-          onAddCreator={addCreator}
-          onNameChange={handleNameChange}
-        />
-        <WorkNameInput register={register} />
-        <Medium register={register} />
-        <FileUpload handleFileUpload={handleFileUpload} />
-        <CustomDatePicker
-          selectedDate={selectedDate}
-          onChange={setSelectedDate}
-        />
-        <LicenseSelect
-          selectedLicense={selectedLicense}
-          handleLicenseChange={handleLicenseChange}
-          handleLicenseUpload={handleFileUpload}
-        />
-        <FileUploadForLicense
-          handleLicenseUpload={handleLicenseUpload}
-          selectedLicense={selectedLicense}
-        />
-        <button className='btn-primary btn w-full' type='submit'>
+        <div className='flex flex-col space-y-4 pb-20'>
+          <CreatorsList
+            creators={creators}
+            onAddCreator={addCreator}
+            onNameChange={handleCreatorName}
+          />
+
+          <WorkNameInput
+            onWorkNameChange={handleWorkName}
+            workInput={workName}
+          />
+
+          <Medium
+            selectedMedium={selectedMedium}
+            handleMediumSelected={handleMediumSelected}
+            handleAlternativeMedium={handleAlternativeMedium}
+            alternativeMedium={alternativeMedium}
+          />
+
+          <FileUpload handleFileUpload={handleFileUpload} />
+          <CustomDatePicker
+            selectedDate={selectedDate}
+            onChange={setSelectedDate}
+          />
+          <LicenseSelect
+            selectedLicense={selectedLicense}
+            handleLicenseChange={handleLicenseChange}
+            handleLicenseUpload={handleLicenseUpload}
+          />
+          <FileUploadForLicense
+            handleLicenseUpload={handleLicenseUpload}
+            selectedLicense={selectedLicense}
+          />
+        </div>
+        <button className='w-full bg-primary py-4 text-white'>
           Submit
         </button>
       </form>
