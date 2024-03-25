0cc604a433ea396a76c3d0f52f8a58ef6452e6ff
Bestin John <bestinjohn1993@gmail.com>
2024-03-25 18:59:21 +0100

    Fixes Build

--- app/assets/icons/index.ts
+++ app/assets/icons/index.ts
@@ -2,16 +2,16 @@ import EvidenceSvg from './Frame';
 import MessageIcon from './MessageIcon';
 import SupportIcon from './annotation';
 import notificationIcon from './bell-01.svg';
+import calenderIcon from './calendericon.svg';
 import dropdownIcon from './dropdownicon.svg';
 import evidencForm from './evidenceform.svg';
+import fileuploadIcon from './fileUpload.svg';
 import HomeBtnSvg from './home';
 import messageIcon from './messagesicon.svg';
 import NotificationIcon from './notificationIcon';
+import searchIcon from './search.png';
 import SingleUser from './singleUser';
-import tooltip from "./tooltip.svg"
-import calenderIcon from "./calendericon.svg"
-import fileuploadIcon from "./fileUpload.svg"
-
+import tooltip from './tooltip.svg';
 
 export {
   EvidenceSvg,
@@ -20,11 +20,12 @@ export {
   NotificationIcon,
   SingleUser,
   SupportIcon,
+  calenderIcon,
   dropdownIcon,
   evidencForm,
+  fileuploadIcon,
   messageIcon,
   notificationIcon,
+  searchIcon,
   tooltip,
-  calenderIcon,
-  fileuploadIcon
 };
