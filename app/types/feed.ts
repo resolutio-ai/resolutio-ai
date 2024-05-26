b8bd3c6bcaebc50b6257bfbe466d90d2676e453a
Anish <anishpraveen7@gmail.com>
2024-05-26 15:14:28 +0200

    Type update

--- app/types/feed.ts
+++ app/types/feed.ts
@@ -1,16 +1,36 @@
-export type FeedItemDto = {
-  id: number;
-  creator: {
-    name: string;
-    image: string;
-  };
-  work: {
-    type: string;
-    url: string;
-    description: string;
-  };
-  createdOn: string;
-  views: number;
-  licenses: number;
-  code: string;
-};
+// export type FeedItemDto = {
+//   id: number;
+//   creator: {
+//     name: string;
+//     image: string;
+//   };
+//   work: {
+//     type: string;
+//     url: string;
+//     description: string;
+//   };
+//   createdOn: string;
+//   views: number;
+//   licenses: number;
+//   code: string;
+// };
+
+export interface FeedItemDto {
+  userId:                string;
+  nameOfWork:            string;
+  medium:                string;
+  timeStamp:             Date;
+  licenseType:           string;
+  cid:                   string;
+  fileUploadResponse:    EUploadResponse;
+  licenseUploadResponse: EUploadResponse;
+  createdAt:             Date;
+  updatedAt:             Date;
+}
+
+export interface EUploadResponse {
+  hash: string;
+  name: string;
+  size: string;
+  _id:  string;
+}
