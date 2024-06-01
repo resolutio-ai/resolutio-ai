c44ff58e10c2b8e91c72b2ba056758eac0196cf9
Bestin John <bestinjohn1993@gmail.com>
2024-06-01 18:07:36 +0200

    chore(deps): Dependency Updates

--- app/types/feed.ts
+++ app/types/feed.ts
@@ -1,37 +1,19 @@
-// export type FeedItemDto = {
-//   id: number;
-//   creator: {
-//     name: string;
-//     image: string;
-//   };
-//   work: {
-//     type: string;
-//     url: string;
-//     description: string;
-//   };
-//   createdOn: string;
-//   views: number;
-//   licenses: number;
-//   code: string;
-// };
-
-export interface FeedItemDto {
-  _id:                   string;
-  userId:                string;
-  nameOfWork:            string;
-  medium:                string;
-  timeStamp:             Date;
-  licenseType:           string;
-  cid:                   string;
-  fileUploadResponse:    EUploadResponse;
-  licenseUploadResponse: EUploadResponse;
-  createdAt:             Date;
-  updatedAt:             Date;
-}
-
 export interface EUploadResponse {
   hash: string;
   name: string;
   size: string;
-  _id:  string;
+  _id: string;
+}
+export interface FeedItemDto {
+  _id: string;
+  userId: string;
+  nameOfWork: string;
+  medium: string;
+  timeStamp: Date;
+  licenseType: string;
+  cid: string;
+  fileUploadResponse: EUploadResponse;
+  licenseUploadResponse: EUploadResponse;
+  createdAt: Date;
+  updatedAt: Date;
 }
