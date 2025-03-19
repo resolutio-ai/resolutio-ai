e22f3c79b3d23a22980613b97793244fa940f5b8
Bestin John <bestinjohn1993@gmail.com>
2025-03-19 21:17:28 +0100

    feat: Refactor DisputeResolutionForm to use FormProvider and add FileUpload component for evidence attachments

--- app/components/dispute-resolution-form/DisputeResolutionForm.tsx
+++ app/components/dispute-resolution-form/DisputeResolutionForm.tsx
@@ -2,20 +2,23 @@
 
 import { disputeFormSchema } from '@/app/schemas/disputeFormSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { ErrorMsg } from '../error-msg/ErrorMsg';
+import FileUpload from '../Evidence/FileUpload/FileUpload';
 
 type DisputeData = z.infer<typeof disputeFormSchema>;
 
 export const DisputeResolutionForm = () => {
+  const methods = useForm<DisputeData>({
+    resolver: zodResolver(disputeFormSchema)
+  });
+
   const {
     handleSubmit,
     register,
     formState: { errors }
-  } = useForm<DisputeData>({
-    resolver: zodResolver(disputeFormSchema)
-  });
+  } = methods;
 
   const onSubmit = (data: DisputeData) => {
     console.log(data);
@@ -27,85 +30,97 @@ export const DisputeResolutionForm = () => {
         Initiate Dispute
       </h3>
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className='mt-4'>
-            <label
-              className='text-sm font-bold text-gray-600'
-              htmlFor='oppositePartyName'
-            >
-              Opposite Party/ Parties
-            </label>
-            <input
-              type='text'
-              className='input input-primary mt-2 w-full'
-              placeholder="Party/parties against whom you're imitating this dispute"
-              id='oppositePartyName'
-              {...register('oppositePartyName')}
-            />
-            <ErrorMsg name={'oppositePartyName'} errors={errors} />
-          </div>
-          <div className='mt-1'>
-            <label className='text-sm font-bold text-gray-600' htmlFor='artId'>
-              Art Id
-            </label>
-            <input
-              type='text'
-              className='input input-primary mt-2 w-full'
-              placeholder='If the art is already recorded on creator armour'
-              id='artId'
-              {...register('artId')}
-            />
-            <ErrorMsg name={'artId'} errors={errors} />
-          </div>
-          <div className='mt-1'>
-            <label
-              className='text-sm font-bold text-gray-600'
-              htmlFor='summary'
-            >
-              Summary
-            </label>
-            <textarea
-              id='summary'
-              rows={10}
-              className='input input-primary mt-2 min-h-[100px] w-full py-2'
-              placeholder='Briefly describe the dispute(250 Charatrers).'
-              {...register('summary')}
-            />
-            <ErrorMsg name={'summary'} errors={errors} />
-          </div>
-          <div className='mt-1'>
-            <label
-              className='text-sm font-bold text-gray-600'
-              htmlFor='caseDetails'
-            >
-              Case Details
-            </label>
-            <input
-              type='text'
-              className='input input-primary mt-2 w-full'
-              placeholder='Case Details'
-              id='caseDetails'
-              {...register('caseDetails')}
-            />
-            <ErrorMsg name={'caseDetails'} errors={errors} />
-          </div>
-          <div className='mt-1'>
-            <label className='text-sm font-bold text-gray-600' htmlFor='type'>
-              Type
-            </label>
-            <input
-              type='text'
-              className='input input-primary mt-2 w-full'
-              placeholder='Dispute Type(Drawing, Design, Music, Illustration, Video, Code, Film, Literature)'
-              id='type'
-              {...register('type')}
-            />
-            <ErrorMsg name={'type'} errors={errors} />
-          </div>
-          <button type='submit' className='btn btn-primary mt-4 w-full'>
-            Submit
-          </button>
-        </form>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='mt-4'>
+              <label
+                className='text-sm font-bold text-gray-600'
+                htmlFor='oppositePartyName'
+              >
+                Opposite Party/ Parties
+              </label>
+              <input
+                type='text'
+                className='input input-primary mt-2 w-full'
+                placeholder="Party/parties against whom you're imitating this dispute"
+                id='oppositePartyName'
+                {...register('oppositePartyName')}
+              />
+              <ErrorMsg name={'oppositePartyName'} errors={errors} />
+            </div>
+            <div className='mt-1'>
+              <label
+                className='text-sm font-bold text-gray-600'
+                htmlFor='artId'
+              >
+                Art Id
+              </label>
+              <input
+                type='text'
+                className='input input-primary mt-2 w-full'
+                placeholder='If the art is already recorded on creator armour'
+                id='artId'
+                {...register('artId')}
+              />
+              <ErrorMsg name={'artId'} errors={errors} />
+            </div>
+            <div className='mt-1'>
+              <label
+                className='text-sm font-bold text-gray-600'
+                htmlFor='summary'
+              >
+                Summary
+              </label>
+              <textarea
+                id='summary'
+                rows={10}
+                className='input input-primary mt-2 min-h-[100px] w-full py-2'
+                placeholder='Briefly describe the dispute(250 Charatrers).'
+                {...register('summary')}
+              />
+              <ErrorMsg name={'summary'} errors={errors} />
+            </div>
+            <div className='mt-1'>
+              <label
+                className='text-sm font-bold text-gray-600'
+                htmlFor='caseDetails'
+              >
+                Case Details
+              </label>
+              <input
+                type='text'
+                className='input input-primary mt-2 w-full'
+                placeholder='Case Details'
+                id='caseDetails'
+                {...register('caseDetails')}
+              />
+              <ErrorMsg name={'caseDetails'} errors={errors} />
+            </div>
+            <div className='mt-1'>
+              <label className='text-sm font-bold text-gray-600' htmlFor='type'>
+                Type
+              </label>
+              <input
+                type='text'
+                className='input input-primary mt-2 w-full'
+                placeholder='Dispute Type(Drawing, Design, Music, Illustration, Video, Code, Film, Literature)'
+                id='type'
+                {...register('type')}
+              />
+              <ErrorMsg name={'type'} errors={errors} />
+            </div>
+            <div>
+              <FileUpload
+                name='evidenceAttachments'
+                label='Attach your evidence'
+              />
+              <ErrorMsg name='evidenceAttachments' errors={errors} />
+            </div>
+            <button type='submit' className='btn btn-primary mt-4 w-full'>
+              Submit
+            </button>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
