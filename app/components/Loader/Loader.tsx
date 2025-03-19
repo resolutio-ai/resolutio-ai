5c895200f0acb85d681ba0088f14570e672d6c67
Bestin John <bestinjohn1993@gmail.com>
2025-03-19 23:37:56 +0100

    refactor: Remove unused Footer, Loader, and Unauthorized components to streamline codebase

--- app/components/Loader/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Loader.module.css';
-export const Loader = () => {
-  return (
-    <div className='flex h-[100%] items-center justify-center'>
-      <span className={styles.loader}></span>
-    </div>
-  );
-};
